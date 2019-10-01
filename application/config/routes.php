0a1ceae194a882b46e7e090c915a65e7f60e5bfa
ahmedsuhailsa <ahmedsuhail@cgvakindia.com>
2019-10-01 15:28:04 +0530

    Added Scoreboard Design

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $route['login'] = 'authentication/login';
+$route['logout'] = 'dashboard/logout';
 $route['default_controller'] = 'authentication';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
