6c65bcf709d83538008b09a0f33f6619be7d9d45
prakash.palanisamy <prakash.palanisamy@gbt-contractors.com>
2019-11-15 21:48:50 +0530

    added access forbidden page

--- application/views/livescore_display.php
+++ application/views/livescore_display.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta http-equiv="refresh" content="5">
-	<title>GPL - Livew Score</title>
+	<title>GPL - Live Score</title>
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<link rel="stylesheet" href="<?php echo site_url('resources/css/bootstrap.min.css');?>">
 	<link rel="stylesheet" href="<?php echo site_url('resources/css/font-awesome.min.css');?>">
